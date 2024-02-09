44d7174184fbeca5f9be7cdb54494922e7ca1a20
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-02-09 08:50:18 +0800

    rearrange code

---
+++ day2009-bouncing-discs/js/update.js
@@ -0,0 +1,16 @@
+function update(ctx, queries, appliers, drawers, debuggers) {
+  for (const id of ctx.entities) {
+    queries.forEach(system => system(id, ctx));
+  }
+  for (const id of ctx.entities) {
+    appliers.forEach(system => system(id, ctx));
+  }
+  for (const id of ctx.entities) {
+    drawers.forEach(system => system(id, ctx));
+  }
+  debuggers.forEach(system => system(ctx));
+  ctx.needQueryAgain[0] = false;
+  ctx.collideNormal.clear();
+  ctx.distanceUntilCollision.clear();
+  ctx.collideWith.clear();
+}
