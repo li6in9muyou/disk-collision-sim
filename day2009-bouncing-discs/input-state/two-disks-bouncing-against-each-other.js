a251f591714f71882dd5ee06636c9eec07740242
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-02-10 23:32:09 +0800

    write input states in a dedicated dir

---
+++ day2009-bouncing-discs/input-state/two-disks-bouncing-against-each-other.js
@@ -0,0 +1,55 @@
+import {
+  applyConservationOfMomentum,
+  applyKeepMovingIfNoCollision,
+  applyMoveToCollidePos,
+  applyReflectedVelocityIfCollideWithArena,
+  drawOrangeDisk,
+  drawVelocityPointer,
+  queryArenaCollision,
+  queryDiskCollision,
+  warnDiskPenetration,
+} from "../js/system.js";
+
+export const config = [
+  [
+    queryArenaCollision,
+    queryDiskCollision,
+  ],
+  new Set([
+    applyReflectedVelocityIfCollideWithArena,
+    applyConservationOfMomentum,
+    applyMoveToCollidePos,
+    applyKeepMovingIfNoCollision,
+  ]),
+  [drawOrangeDisk, drawVelocityPointer],
+  [warnDiskPenetration],
+];
+
+export const init = (ARENA_W, ARENA_H) => ({
+  entities: [1000, 1001],
+  velocity: new Map([
+    [1000, { x: 0, y: 40 }],
+    [1001, { x: 0, y: -40 }],
+  ]),
+  position: new Map([
+    [1000, { x: ARENA_W / 2, y: 20 }],
+    [1001, { x: ARENA_W / 2, y: ARENA_H - 40 }],
+  ]),
+  size: new Map([
+    [1000, { w: 40, h: 40 }],
+    [1001, { w: 80, h: 80 }],
+  ]),
+  mass: new Map([
+    [1000, 40],
+    [1001, 160],
+  ]),
+  elapsed: 0,
+  collideNormal: new Map(),
+  distanceUntilCollision: new Map(),
+  timeUntilCollision: new Map(),
+  collideWith: new Map(),
+  vPtrJqTable: new Map(),
+  needQueryAgain: [true],
+  ARENA_W,
+  ARENA_H,
+});
